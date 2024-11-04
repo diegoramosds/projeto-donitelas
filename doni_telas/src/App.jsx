00ad3dc0f13e4d7b4edcadd9beddf2fadffc07e5
Diego <diegoramos3709@gmail.com>
2024-11-03 22:32:07 -0300

    Configurando react router

---
+++ doni_telas/src/App.jsx
@@ -0,0 +1,34 @@
+// Import all of Bootstrap's JS
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+//pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import NavBar from './components/NavBar/NavBar'
+import Work from './pages/Work/Work'
+
+// index.js ou App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+     <BrowserRouter>
+     <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        
+      </Routes>
+    </BrowserRouter>
+        
+    </>
+  )
+}
+
+export default App
