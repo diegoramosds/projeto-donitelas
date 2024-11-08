4c6783b3c80fd3338973910ed113f4dff0d93b28
Diego <140274064+diegoramosds@users.noreply.github.com>
2024-11-08 19:07:28 -0300

    Estilizando Home

--- doni_telas/src/pages/Home/Home.jsx
+++ doni_telas/src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
+import CountUp from 'react-countup';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <div className='main-navbar'>
-      <div className="info-navbar">
-        <h1>Trabalhamos com amor e dedicação para construir com excelência</h1>
-        <p>Servços em geral</p>
+    <div className='main-home'>
+      <div className="info-home">
+        <h1>Trabalho com amor e dedicação para construir com excelência</h1>
+        <p className="important-words">Instalação e Manutenção de Telas | Cercas e Grades de Segurança 
+          | Serviços de Construção e Reformas de quadras </p>
       </div>
+      <div className="counters">
+         <p>+<CountUp start={0} end={500} duration={4} /> Clientes satisfeitos</p>
+         <p>+<CountUp start={0} end={500} duration={4} /> Serviços feitos</p>
+      </div>
+        <div className='link-home'>
+          <Link to="/work">Saiba mas</Link>
+        </div>
       </div>
   )
 }
