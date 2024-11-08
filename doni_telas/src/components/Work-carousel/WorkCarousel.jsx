06b3e5e91bde0de3a0c85960e8301bfc48735c89
Diego <140274064+diegoramosds@users.noreply.github.com>
2024-11-08 19:06:51 -0300

    Page work

---
+++ doni_telas/src/components/Work-carousel/WorkCarousel.jsx
@@ -0,0 +1,112 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+// images
+import work1 from '../../img/work1.jpg';
+import work2 from '../../img/work2.jpg';
+import work3 from '../../img/work3.jpg';
+import work4 from '../../img/work4.jpg';
+import work5 from '../../img/work5.jpg';
+import work6 from '../../img/work6.jpg';
+import work7 from '../../img/work7.jpg';
+import work8 from '../../img/work8.jpg';
+import work9 from '../../img/work9.jpg';
+import work10 from '../../img/work10.jpg';
+
+
+function IndividualIntervalsExample() {
+  return (
+    <div>
+      <h3>Alguns serviços feitos</h3>
+      <Carousel slide={false}>
+        <Carousel.Item>
+          <img src={work1} alt="" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work2} alt="" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work3} alt="" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work4} alt="" />
+          <Carousel.Caption>
+            <h3>4 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work5} alt="" />
+          <Carousel.Caption>
+            <h3>5 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work6} alt="" />
+          <Carousel.Caption>
+            <h3>6 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work7} alt="" />
+          <Carousel.Caption>
+            <h3>7 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work8} alt="" />
+          <Carousel.Caption>
+            <h3>8 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work9} alt="" />
+          <Carousel.Caption>
+            <h3>9 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={work10} alt="" />
+          <Carousel.Caption>
+            <h3>10 slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default IndividualIntervalsExample;
--- doni_telas/src/pages/Work/Work.jsx
+++ doni_telas/src/pages/Work/Work.jsx
@@ -1,8 +1,72 @@
-
+import { Link } from "react-router-dom"
+import IndividualIntervalsExample from "../../components/Work-carousel/WorkCarousel"
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className="main-work">
+       <h3>Serviços prestados</h3>
+      <div className="details-work">
+        <div className="text-work">
+          <h4>Telas</h4>
+          <p>Ofereço uma variedade de opções para instalação e manutenção de telas:</p>
+             <ul>
+                <li>Instalação de telas de proteção para áreas residenciais e comerciais</li>
+                <li>Manutenção preventiva e reparo de telas desgastadas</li>
+                <li>Modelos de telas sob medida para diferentes espaços</li>
+                <li>Serviços de ajuste e fixação de telas</li>
+             </ul>
+        </div>
+        <div className="text-work">
+        <h4>Cercas</h4>
+          <p>Soluções para cercas de segurança e design:</p>
+        <ul>
+          <li>Instalação de cercas de proteção em áreas industriais e residenciais</li>
+          <li>Opções de cercas personalizadas para proteção</li>
+          <li>Reparos e reforços para cercas de madeira e metal</li>
+          <li>Instalação de cercas para áreas externas e jardins</li>
+        </ul>
+        </div>
+        <div className="text-work">
+        <h4>Grades</h4>
+        <p>Instalação de grades para diferentes ambientes:</p>
+          <ul>
+            <li>Grades de segurança para janelas, portas e varandas</li>
+            <li>Grades decorativas personalizadas em diversos modelos</li>
+            <li>Reforço e manutenção de grades já instaladas</li>
+            <li>Instalação de grades para áreas comerciais e residenciais</li>
+          </ul>
+        </div>
+        <div className="text-work">
+          <h4>Quadras esportivas</h4>
+        <p>Serviços especializados para quadras e áreas de recreação:</p>
+          <ul>
+            <li>Instalação de telas e redes de proteção para quadras esportivas</li>
+            <li>Montagem de cercas e divisórias em quadras</li>
+            <li>Manutenção e reparos em redes esportivas</li>
+            <li>Personalização de redes e cercas para quadras de diferentes esportes</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="info-work">
+        <p>Cada serviço que ofereço é cuidadosamente planejado e adaptado às 
+          características do ambiente e às preferências de nossos clientes,
+           sempre com foco em resultados que garantam segurança e satisfação.</p>
+      </div>
+
+      <div className="carousel-work">
+       <IndividualIntervalsExample />
+      </div>
+
+      <div className="">
+        <p></p>
+      </div>
+      
+      <div className="redirect-work">
+        <p>Gostou e tem interesse? <a href="https://wa.me/5511968579924?text=Olá,%20gostaria%20de%20solitar%20um%20orçamento" target="_blank">CLIQUE AQUI</a> e solicite seu orçamento! </p>
+    
+      </div>
+    </div>
   )
 }
 
