7fb9fb12e2a79792cff589baf39bfd6ce0eb2d67
Diego <diegoramos3709@gmail.com>
2025-03-23 20:34:30 -0300

    Update Home.jsx

--- doni_telas/src/pages/Home/Home.jsx
+++ doni_telas/src/pages/Home/Home.jsx
@@ -1,27 +1,41 @@
-import CountUp from 'react-countup';
-import { FaTools } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 const Home = () => {
   return (
     <div className='main-home'>
-      <div className="info-home">
-        <h1>Trabalho com amor e dedicação para construir com excelência</h1>
-        <p className="important-words">Instalação e Manutenção de Telas | Cercas e Grades de Segurança 
-          | Serviços de Construção e Reformas de quadras </p>
+      <div className='primary'>
+        <div className="introduction-home">
+          <h1>DONI<span className="important-words">TELAS</span></h1>
+          <p>Instalação e Manutenção de Telas | Cercas e Grades de Segurança
+            | Serviços de Construção e Reformas de quadras </p>
+        </div>
+        <div>
+        </div>
+        <div className='options'>
+            <p>Solicitar orçamento</p>
+            <p>Meus serviços</p>
+        </div>
       </div>
-      <div>
+
+
+      <div className="info-profile">
+        <div className="board-details">
+          <p><span><FaUser /></span> +599 <span>Clientes satisfeitos</span></p>
+        </div>
+
+        <div className="board-details">
+          <p><span><FaUser /></span> +599 <span>Clientes satisfeitos</span></p>
+        </div>
+
+        <div className="board-details">
+          <p><span><FaUser /></span> +599 <span>Clientes satisfeitos</span></p>
+        </div>
       </div>
-      <div className="counters">
+
+      {/* <div className="counters">
          <p>+<CountUp start={0} end={599} duration={4} /> Clientes satisfeitos</p>
          <p>+<CountUp start={0} end={599} duration={4} /> Serviços realizados</p>
-      </div>
-      <div className='icon-home'>
-        <FaTools />
-      </div>
-        <div className='link-home'>
-          <Link to="/about"> CLIQUE AQUI </Link> e saiba mais!
-        </div>
+      </div> */}
       </div>
   )
 }
