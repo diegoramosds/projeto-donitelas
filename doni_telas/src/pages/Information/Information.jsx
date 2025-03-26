b922d1524042b238167a480e1cc74c6b156e679d
Diego <diegoramos3709@gmail.com>
2025-03-25 21:27:32 -0300

    Ajustando estilos

--- doni_telas/src/pages/Information/Information.jsx
+++ doni_telas/src/pages/Information/Information.jsx
@@ -1,14 +1,48 @@
-import TextControls from '../../components/Information-form/Information-form'
 
 const Information = () => {
   return (
     <div className='main-info'>
-      <p>Caso tenha <span className='important-words'>duvidas</span> ou <span className='important-words'>perguntas</span>, 
-       entre em contato agora mesmo, pelo formulário abaixo ou 
-        acesse a aba de contato e 
-        venha saber mais!</p>
-     
-        <TextControls />
+      <div className='primary-info'>
+        <h1>Mais informações</h1>
+        <p>Recursos adicionais e informações úteis sobre nossos produtos e serviços</p>
+      </div>
+
+      <div className='questions-info'>
+        <div className="text-question">
+          <h1>Perguntas Frequentes</h1>
+          <p>Encontre respostas para as dúvidas mais comuns sobre nossos produtos e serviços.</p>
+        </div>
+        <div className="ansewrs-question">
+
+          <div className="ansewrs">
+            <h4>Qual o material utilizado nas telas?</h4>
+            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
+              intempéries, garantindo durabilidade e segurança.
+              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+          </div>
+
+          <div className="ansewrs">
+            <h4>Qual o material utilizado nas telas?</h4>
+            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
+              intempéries, garantindo durabilidade e segurança.
+              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+          </div>
+
+          <div className="ansewrs">
+            <h4>Qual o material utilizado nas telas?</h4>
+            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
+              intempéries, garantindo durabilidade e segurança.
+              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+          </div>
+
+          <div className="ansewrs">
+            <h4>Qual o material utilizado nas telas?</h4>
+            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
+              intempéries, garantindo durabilidade e segurança.
+              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+          </div>
+        </div>
+      </div>
     </div>
     
   )
