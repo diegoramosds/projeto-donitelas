7b513efcf834aa79d03f95cf2114f6d731101b6d
Diego <diegoramos3709@gmail.com>
2025-03-27 20:07:26 -0300

    Update Information.jsx

--- doni_telas/src/pages/Information/Information.jsx
+++ doni_telas/src/pages/Information/Information.jsx
@@ -14,37 +14,57 @@ const Information = () => {
         </div>
         <div className="ansewrs-question">
 
-          <div className="ansewrs">
-            <h4>Qual o material utilizado nas telas?</h4>
-            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
-              intempéries, garantindo durabilidade e segurança.
-              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+        <div className="ansewrs">
+            <h4>Quanto é cobrado no orçamento?</h4>
+            <p> O orçamento é totalmente  gratuito  e sem compromisso. Meu objetivo é que você
+              tenha clareza sobre o que será feito e
+              quanto vai investir, permitindo que tome uma decisão  informada   e
+              segura.</p>
           </div>
 
           <div className="ansewrs">
-            <h4>Qual o material utilizado nas telas?</h4>
-            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
-              intempéries, garantindo durabilidade e segurança.
-              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+            <h4>Qual valor cobrado nos serviços?</h4>
+            <p>Os valores variam de acordo com o tipo de serviço e as
+          necessidades  específicas de cada cliente.
+          Cada projeto é único, e por isso, é importante solicitar um  orçamento
+          personalizado. Dessa forma, conseguimos
+          avaliar todas as exigências e oferecer um preço justo e competitivo que atenda ao seu orçamento.</p>
           </div>
 
           <div className="ansewrs">
-            <h4>Qual o material utilizado nas telas?</h4>
-            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
-              intempéries, garantindo durabilidade e segurança.
-              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+            <h4>Quais locais você trabalha?</h4>
+            <p>Atendo em diversas localidades na  região de São Paulo ,
+            incluindo cidades próximas como Sorocaba, Cotia, Mairinque, entre várias outras.
+            Se sua cidade não está na lista,  entre em contato  para verificar a
+            possibilidade de atendimento e esclarecer dúvidas sobre nossa cobertura.</p>
           </div>
 
           <div className="ansewrs">
-            <h4>Qual o material utilizado nas telas?</h4>
-            <p>Nossas telas de proteção são fabricadas com poliéster de alta resistência, tratado contra raios UV e
-              intempéries, garantindo durabilidade e segurança.
-              O material é testado para suportar impactos e resistir ao tempo sem perder suas propriedades.</p>
+            <h4>Como funciona a parte do pagamento?</h4>
+            <p>Normalmente, o pagamento é dividido em duas etapas: uma parte é recebida no início do projeto
+          para cobrir materiais e preparação, e o restante é pago
+          ao final, após a  conclusão do serviç.
+          Essa divisão garante transparência e segurança para ambas as partes.</p>
+          </div>
+
+          <div className="ansewrs">
+            <h4>Quanto tempo é levado para finalizar os serviços?</h4>
+            <p>O tempo necessário para concluir o serviço depende da complexidade
+          e das especificidades
+          de cada projeto, além da localidade. Em geral, oferecemos uma estimativa durante o orçamento 
+          inicial para que você saiba o que esperar e possa se planejar melhor.</p>
+          </div>
+
+          <div className="ansewrs">
+            <h4>Porque escolher você?</h4>
+            <p>Com  anos de experiência no mercado, ofereço serviços com alta qualidade e
+              um  padrão de excelência, sempre focado em atender às necessidades de segurança
+              e estética dos nossos clientes. Meu compromisso é entregar resultados confiáveis e duradouros,
+              garantindo a satisfação de quem confia em nosso trabalho.</p>
           </div>
         </div>
       </div>
     </div>
-    
   )
 }
 
