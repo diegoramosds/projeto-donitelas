d6ebd8d3b3ac9363957015f059857d064456cdf6
Diego <diegoramos3709@gmail.com>
2025-03-28 20:36:54 -0300

    Ajustando estilos

--- doni_telas/src/pages/Home/Home.jsx
+++ doni_telas/src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import { Users, Award, ArrowRight, Phone, Activity } from 'lucide-react';
 import CountUp from 'react-countup';
-import work10 from '../../img/work10.jpg';
 
 import { motion } from "motion/react"
+import { Link } from "react-router-dom"
+
+import ServiceCards from '../../components/ServiceCards/ServiceCards';
+import { featuredServices } from "../../data/ItemFeaturedServices"
 
 const Home = () => {
   return (
@@ -33,8 +36,8 @@ const Home = () => {
         </div>
 
         <div className='options'>
-          <button className='request-quote-button'>Solicitar orçamento</button>
-          <button className='my-services-button'>Meus serviços</button>
+          <button className='request-quote-button'><Link to='/contact'>Solicitar orçamento</Link> </button>
+          <button className='my-services-button'><Link to='/work'>Meus serviços</Link></button>
         </div>
 
         </motion.div>
@@ -82,91 +85,26 @@ const Home = () => {
       </div>
 
       <div className='important-works'>
-        <h1>Serviços em Destaque</h1>
+        <h1>Tipos de Serviços</h1>
         <p>Soluções de proteção completas para sua residência ou negócio.</p>
-        <div className='service-details'>
-
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.1 }}
-          className="service-cards"
-          >
-          <div className='service-images'>
-                        <img src={work10} alt="" />
-                        </div>
-                      <div className='text-details'>
-                        <h1>
-                          Telas de Proteção
-                        </h1>
-                        <p>Proteja crianças e animais com nossas telas de segurança para janelas, sacadas e varandas</p>
-                      </div>
-                      <div className='learn-more'>
-                        <button>
-                          <span>Saiba mais</span>
-                          <span><ArrowRight /></span>
-                          </button>
-                      </div>
-
-          </motion.div>
-
-          <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.3 }}
-          className="service-cards"
-          >
-          <div className='service-images'>
-                        <img src={work10} alt="" />
-                        </div>
-                      <div className='text-details'>
-                        <h1>
-                          Telas de Proteção
-                        </h1>
-                        <p>Proteja crianças e animais com nossas telas de segurança para janelas, sacadas e varandas</p>
-                      </div>
-                      <div className='learn-more'>
-                        <button>
-                          <span>Saiba mais</span>
-                          <span><ArrowRight /></span>
-                          </button>
-                      </div>
-
-          </motion.div>
-
-          <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.5 }}
-          className="service-cards"
-          >
-          <div className='service-images'>
-                        <img src={work10} alt="" />
-                        </div>
-                      <div className='text-details'>
-                        <h1>
-                          Telas de Proteção
-                        </h1>
-                        <p>Proteja crianças e animais com nossas telas de segurança para janelas, sacadas e varandas</p>
-                      </div>
-                      <div className='learn-more'>
-                        <button>
-                          <span>Saiba mais</span>
-                          <span><ArrowRight /></span>
-                          </button>
-                      </div>
-          </motion.div>
+        <div>
+        <ServiceCards
+      textButtonCloseFeatures='Saiba Mais'
+      textButtonOpenFeatures='Ocultar Detalhes'
+      titleFeatures='Características'
+      iconButton={<ArrowRight />}
+      itemsMap={featuredServices}
+      />
         <div>
       </div>
     </div>
         <div className='see-all-services'>
               <button>
-                <span>Ver todos os serviços</span>
+              <Link to='/work'><span>Ver todos os serviços</span></Link>
                 <span><ArrowRight /></span>
                 </button>
             </div>
       </div>
-
       <div className='contact'>
       <motion.div
           initial={{ opacity: 0, y: 30 }}
@@ -177,7 +115,7 @@ const Home = () => {
           >
                   <h1>Solicite um Orçamento Grátis</h1>
                   <p>Entre em contato por WhatsApp para solicitar um orçamento sem compromisso ou agende uma visita técnica.</p>
-                  <button> <span><Phone /> </span> <span>Chamar no WhataApp</span></button>
+                  <button> <span><Phone /> </span> <span><a href="https://wa.me/5511968579924?text=Olá,%20gostaria%20de%20solitar%20um%20orçamento" target="_blank">Chamar no WhataApp</a></span></button>
           </motion.div>
 
       </div>
